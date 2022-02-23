811dcf2356a2b3f73d4d1c220268ec73b0863e61
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-23 16:53:49 -0300

    requisito 1: renderiza componente Input dentro do componente Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Input from './Input';
 
 class Form extends Component {
   render() {
     return (
-      <div>Formulário</div>
+      <div>
+        <Input label="Nome" type="text" id="name-input" />
+        <Input label="Descrição" type="textarea" id="description-input" />
+        <Input label="Attr01" type="number" id="attr1-input" />
+        <Input label="Attr02" type="number" id="attr2-input" />
+        <Input label="Attr03" type="number" id="attr3-input" />
+        <Input label="Imagem" type="text" id="image-input" />
+        <Input label="Raridade" type="select" id="rare-input" />
+        <Input label="Super Trybe Trunfo" type="checkbox" id="trunfo-input" />
+        <Input label="Salvar" type="button" id="save-button" />
+      </div>
     );
   }
 }
