003130b625f037c55d966e5764f87b630a864c4e
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-24 14:12:41 -0300

    requisito 3: valida componente Card com PropTypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends Component {
   render() {
@@ -30,5 +31,16 @@ class Card extends Component {
     );
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
 
 export default Card;
