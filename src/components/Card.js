60a45b11d76e8a2968c3729fcd9558bb05b1f33a
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-24 14:11:01 -0300

    requisito 3: cria componente Card

---
+++ src/components/Card.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+    return (
+      <div>
+        <h2 data-testid="name-card">{ cardName }</h2>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <p data-testid="attr1-card">{ cardAttr1 }</p>
+        <p data-testid="attr2-card">{ cardAttr2 }</p>
+        <p data-testid="attr3-card">{ cardAttr3 }</p>
+        <p data-testid="rare-card">{ cardRare }</p>
+        {
+          cardTrunfo === true
+            ? (<p data-testid="trunfo-card">Super Trunfo</p>)
+            : null
+        }
+      </div>
+    );
+  }
+}
+
+export default Card;
