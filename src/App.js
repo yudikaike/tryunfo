bcd6bedb79453c0d0b1757cc16c1c4e1d86b7349
Kaike Yudi <kaikeyudi@outlook.com>
2022-02-24 17:21:50 -0300

    requisito 8: renderiza a lista de cartas no componente principal

--- src/App.js
+++ src/App.js
@@ -148,6 +148,7 @@ class App extends React.Component {
       cardTrunfo,
       hasTrunfo,
       isSaveButtonDisabled,
+      cardCatalog,
     } = this.state;
 
     return (
@@ -176,6 +177,19 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {cardCatalog.map((card, index) => (
+          <Card
+            key={ index }
+            cardName={ card.cardName }
+            cardDescription={ card.cardDescription }
+            cardAttr1={ card.cardAttr1 }
+            cardAttr2={ card.cardAttr2 }
+            cardAttr3={ card.cardAttr3 }
+            cardImage={ card.cardImage }
+            cardRare={ card.cardRare }
+            cardTrunfo={ card.cardTrunfo }
+          />
+        ))}
       </div>
     );
   }
